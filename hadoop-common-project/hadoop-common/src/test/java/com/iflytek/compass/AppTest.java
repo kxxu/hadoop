4c331499bb411d1422f6da62bb413a27a69e7279
kexu xu <kexu xu>
2016-05-15 21:49:48 +0800

    common test

---
+++ hadoop-common-project/hadoop-common/src/test/java/com/iflytek/compass/AppTest.java
@@ -0,0 +1,12 @@
+package com.iflytek.compass;
+
+/**
+ * Created by admin on 2016/5/15.
+ */
+public class AppTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+
+}
